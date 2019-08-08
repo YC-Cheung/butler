556cf3f7ad9e9f211dbedd13df1ceb1caba61ea6
YC <zzero591619@gmail.com>
2019-08-08 03:11:05 +0000

    add generate token command

---
+++ app/Console/Commands/GenerateToken.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class GenerateToken extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'butler:generate-token';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate JWT token for authorization';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $userType = $this->choice('Please choose the account type', ['admin', 'user']);
+        $userId = $this->ask('Please enter a user id');
+        $days = $this->ask('Please enter a expired days');
+
+        $userModel = $userType === 'admin' ? 'App\Models\Admin' : 'App\Models\User';
+
+        $user = $userModel::find($userId);
+        if (!$user) {
+            $this->warn('The user not found');
+            return;
+        }
+
+        $ttl = (int)$days * 24 * 60;
+        JWTAuth::factory()->setTTL($ttl);
+        $token = JWTAuth::fromUser($user);
+
+        $this->info($token);
+    }
+}
