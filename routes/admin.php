6aa616bd0695cbc4a34c531e3ccb1b1660e7a89a
YC <zzero591619@gmail.com>
2019-08-19 20:30:11 +0800

    add vue SPA template

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,8 @@
 <?php
-Route::get('test', 'IndexController@test');
-Route::post('login', 'AuthController@login');
 
+Route::post('login', 'AuthController@login');
 Route::middleware([
+    'auth:admin'
 ])->group(function () {
     Route::apiResource('users', 'UserController')->names('admin.users');
     Route::apiResource('roles', 'RoleController')->names('admin.roles');
